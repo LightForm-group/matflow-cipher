48f87f4a43fb9e57d56a78c011b91c40234fb2df
aplowman <adam.jp@live.co.uk>
2022-07-06 21:19:16 +0100

    initial commit

---
+++ matflow_cipher/main.py
@@ -0,0 +1,10 @@
+'`matflow_cipher.main.py`'
+
+from matflow_cipher import (
+    input_mapper,
+    output_mapper,
+    cli_format_mapper,
+    register_output_file,
+    func_mapper,
+    software_versions,
+)
