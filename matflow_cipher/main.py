f9489760a6ce6623cf3668e1a07dbdff1639ffa0
aplowman <adam.jp@live.co.uk>
2022-07-06 22:08:15 +0100

    feat: generate_phase_field_input/random_voronoi

--- matflow_cipher/main.py
+++ matflow_cipher/main.py
@@ -1,10 +1,59 @@
-'`matflow_cipher.main.py`'
+"`matflow_cipher.main.py`"
 
-from matflow_cipher import (
-    input_mapper,
-    output_mapper,
-    cli_format_mapper,
-    register_output_file,
-    func_mapper,
-    software_versions,
+from black import out
+from cipher_parse._version import __version__ as cipher_parse_version
+from cipher_parse.cipher_input import (
+    CIPHERInput,
+    MaterialDefinition,
+    InterfaceDefinition,
+    PhaseTypeDefinition,
 )
+
+from matflow_cipher import func_mapper, software_versions
+
+
+@func_mapper(task="generate_phase_field_input", method="from_random_voronoi")
+def generate_phase_field_input_from_random_voronoi(
+    materials,
+    interfaces,
+    num_phases,
+    grid_size,
+    size,
+    components,
+    outputs,
+    solution_parameters,
+    random_seed,
+):
+    mats = []
+    for mat_i in materials:
+        if "phase_types" in mat_i:
+            mat_i["phase_types"] = [
+                PhaseTypeDefinition(**j) for j in mat_i["phase_types"]
+            ]
+            mat_i = MaterialDefinition(**mat_i)
+        mats.append(mat_i)
+
+    interfaces = [InterfaceDefinition(**int_i) for int_i in interfaces]
+
+    inp = CIPHERInput.from_random_voronoi(
+        materials=mats,
+        interfaces=interfaces,
+        num_phases=num_phases,
+        grid_size=grid_size,
+        size=size,
+        components=components,
+        outputs=outputs,
+        solution_parameters=solution_parameters,
+        random_seed=random_seed,
+    )
+    return {"phase_field_input": inp.to_JSON()}
+
+
+@software_versions()
+def get_versions():
+    "Get versions of pertinent software associated with this extension."
+
+    out = {
+        "cipher-parse (Python)": {"version": cipher_parse_version},
+    }
+    return out
